c45713c95f50e483d65216278c71b2985da775a8
ShiZhan <g.shizhan.g@gmail.com>
2016-03-31 14:28:43 +0800

    rand() will loop at 2^27

--- src/rmat.cpp
+++ src/rmat.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 #include <ctime>
 #include <stdint.h>
 #include "options.h"
@@ -18,17 +19,18 @@ int main (int argc, char* argv[]) {
 
 	int scale  = getValue(argv, argv + argc, "-s", 8);
 	int degree = getValue(argv, argv + argc, "-d", 8);
-	uint32_t seed = getValue(argv, argv + argc, "-r", (uint32_t)time(NULL));
+	int seed   = getValue(argv, argv + argc, "-r", time(NULL));
 
-	srand(seed);
+	mt19937_64 gen(seed);
+	uniform_int_distribution<uint64_t> ud(0, 0xffffffffffffffff);
 
-	int r = RAND_MAX, b4 = 0, b4_per_rand = 0, nb4 = 0;
-	while ((r & 0xf) == 0xf) { b4_per_rand++; r >>= 4; }
-	
+	uint64_t r, u, v;
+	uint8_t b4 = 0, b4_per_rand = 16, nb4 = 0;
+
 	for (uint64_t e = (1 << scale) * degree; e > 0; e--) {
-		uint64_t u = 0, v = 0;
+		u = 0, v = 0;
 		for (int i = 0; i < scale; i++) {
-			if (nb4 == 0) { r = rand(); nb4 = b4_per_rand; }
+			if (nb4 == 0) { r = ud(gen); nb4 = b4_per_rand; }
 			b4 = r & 0xf;
 			u <<= 1; v <<= 1;
 			if (b4 == 0) { u++; v++; }
