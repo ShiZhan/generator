b675c620aafe04a686ffc2959f4b2ef82828ecb2
Shi.Zhan <g.shizhan.g@gmail.com>
2019-11-25 23:33:53 +0800

    synthesis script variables

--- src/cloned-subgraph-synthesis.sh
+++ src/cloned-subgraph-synthesis.sh
@@ -2,9 +2,11 @@ S0=4
 D0=4
 S1=4
 D1=4
-IDS0=`seq 0 $[ (1 << S0) - 1 ]`
-MAP_FILE="vertices.map"
-# MAP_FILE="/dev/null"
+MAX_ID0=$(( (1 << S0) - 1 ))
+IDS0=`seq 0 $MAX_ID0`
+EDGE_FILE="r0-$S0-$D0-r1-$S1-$D1.edges"
+DICT_FILE="$EDGE_FILE.dict"
+# DICT_FILE="/dev/null"
 cat \
     <(./rmat -s $S1 -d $D1 | while read u v
     do
@@ -18,12 +20,12 @@ cat \
         echo $u"#0 "$v"#0"
     done) | \
 shuf | \
-awk -v map_file=$MAP_FILE 'BEGIN { i=0 } {
+awk -v dict_file=$DICT_FILE 'BEGIN { i=0 } {
     if (!($1 in dict)) dict[$1]=i++
     if (!($2 in dict)) dict[$2]=i++
     print dict[$1]" "dict[$2]
 } END {
     for (i in dict) {
-        print i" "dict[i] > map_file
+        print i" "dict[i] > dict_file
     }
-}' > graph.edges
+}' > $EDGE_FILE
