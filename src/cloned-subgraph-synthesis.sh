a1cf2a3e9f7039202fe88690f085e9d86262120c
Shi.Zhan <g.shizhan.g@gmail.com>
2019-11-26 15:04:35 +0800

    simplify

--- src/cloned-subgraph-synthesis.sh
+++ src/cloned-subgraph-synthesis.sh
@@ -11,18 +11,17 @@ EDGE_FILE="r0-$S0-$D0-r1-$S1-$D1.edges"
 DICT_FILE="$EDGE_FILE.dict"
 # DICT_FILE="/dev/null"
 cat \
+    <(./rmat -s $S0 -d $D0 | while read u v
+    do
+        echo $u"#0 "$v"#0"
+    done) \
     <(./rmat -s $S1 -d $D1 | while read u v
     do
         for i in $IDS0
         do
             echo "$i#"$u" $i#"$v
         done
-    done) \
-    <(./rmat -s $S0 -d $D0 | while read u v
-    do
-        echo $u"#0 "$v"#0"
     done) | \
-shuf | \
 awk -v dict_file=$DICT_FILE 'BEGIN { i=0 } {
     if (!($1 in dict)) dict[$1]=i++
     if (!($2 in dict)) dict[$2]=i++
